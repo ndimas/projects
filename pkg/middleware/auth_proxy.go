869dc819542e0de23bbd76740efbc2ba9f6dc35b
georgeoly <george.olymbiou@gmail.com>
2015-12-17 13:33:08 +0200

    added lottos auth with validation on json token

--- pkg/middleware/auth_proxy.go
+++ pkg/middleware/auth_proxy.go
@@ -7,6 +7,7 @@ import (
   "github.com/dgrijalva/jwt-go"
 	"github.com/grafana/grafana/pkg/setting"
   "errors"
+  "time"
 )
 
 func initContextWithAuthProxy(ctx *Context) bool {
@@ -24,21 +25,22 @@ func initContextWithAuthProxy(ctx *Context) bool {
 
 	if len(proxyHeaderValue) == 0 {
       log.Info("auth_proxy.go ::: proxyHeaderValue returned null")
+      //TODO: Redirect or Access is denied
 			return false
 	}
 
-  //TODO: Validate Token
-  valid := isValidToken(proxyHeaderValue)//isValidToken(proxyHeaderValue, func() {  })
+  valid, username, authorities := isValidToken(proxyHeaderValue)//isValidToken(proxyHeaderValue, func() {  })
 
   if ( !valid ) {
     log.Debug("auth_proxy.go ::: isValidToken returning %v", valid)
-
 
     ctx.Handle(401, "Access is denied", errors.New("Access is denied"))
     return false
   }
 
-	query := getSignedInUserQueryForProxyAuth(proxyHeaderValue)
+  log.Debug("auth_proxy.go ::: going to validate username %v and authorities %v", username, authorities)
+
+	query := getSignedInUserQueryForProxyAuth(username)
 	if err := bus.Dispatch(query); err != nil {
 		if err != m.ErrUserNotFound {
 			ctx.Handle(500, "Failed find user specifed in auth proxy header", err)
@@ -100,51 +102,68 @@ func getCreateUserCommandForProxyAuth(headerVal string) *m.CreateUserCommand {
 	return &cmd
 }
 
-func myLookupKey(token interface {}) (interface{}, error) {
+func isExpired(exp_timestamp int64) bool {
+  return time.Now().Unix() > exp_timestamp
+}
+
+func lookupCallback(token map[string]interface{}) (interface{}, error) {
 
   log.Debug("auth_proxy.go ::: entered myLookupKey for token %v", token)
+
+  log.Debug("auth_proxy.go ::: client_id=%v, jti=%v, scope=%v, exp=%v, user_name=%v, authorities=%v",
+
+    token["client_id"],token["jti"],token["scope"],token["exp"],token["user_name"],token["authorities"])
+
+  expirationValue := token["exp"].(int64)
+
+  if isExpired ( expirationValue ) {
+    return nil, errors.New("auth_proxy.go ::: exp value on json object expired !!!")
+  }
 
   //on error return nil, errors.New("foobar")
 
   return token, nil
 }
 
-func isValidToken(inputToken string) bool {
+func isValidToken(inputToken string) (bool, string, interface{}) {
 
   if len(inputToken) == 0 {
     log.Debug("auth_proxy.go ::: nil input token found")
-    return false
+    return false, "", nil
   }
 
   token, err := jwt.Parse(inputToken, func(token *jwt.Token) (interface{}, error) {
 
     log.Debug("auth_proxy.go ::: callback for token %v", token)
 
-
-    return myLookupKey(token.Header["kid"])
+    return lookupCallback(token.Claims)
   })
 
   if token == nil {
     log.Debug("auth_proxy.go ::: no token found ")
-    return false
+    return false, "", nil
   }
 
   if token.Valid {
     log.Debug("auth_proxy.go ::: Token Validation Success!")
-    return true
+
+    user_name   := token.Claims["user_name"].(string)
+    authorities := token.Claims["authorities"]
+
+    return true, user_name, authorities
   } else if ve, ok := err.(*jwt.ValidationError); ok {
     if ve.Errors&jwt.ValidationErrorMalformed != 0 {
       log.Debug("auth_proxy.go ::: Cannot recognize the token")
-      return false
+      return false, "", nil
     } else if ve.Errors&(jwt.ValidationErrorExpired |jwt.ValidationErrorNotValidYet) != 0 {
       log.Debug("auth_proxy.go ::: Token Expired or not active yet")
-      return false
+      return false, "", nil
     } else {
       log.Debug("auth_proxy.go ::: Couldn't handle token")
-      return false
+      return false, "", nil
     }
   } else {
     log.Debug("auth_proxy.go ::: Couldn't handle token")
-    return false
+    return false, "", nil
   }
 }
